f1b0316ef0e2258f879b66cc6118830c6ad03c09
YA <9039417+yzkn@users.noreply.github.com>
2024-07-04 05:30:15 +0900

    init

---
+++ GPSLogger2/GPSLogger2App.swift
@@ -0,0 +1,17 @@
+//
+//  GPSLogger2App.swift
+//  GPSLogger2
+//
+//  Created by Yu on 2024/07/04.
+//
+
+import SwiftUI
+
+@main
+struct GPSLogger2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Swift-iOS-GPSLogger2
- GPSLogger2
+
+---
+
+Copyright (c) 2024 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
