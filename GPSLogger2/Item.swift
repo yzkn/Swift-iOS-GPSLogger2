a7a94e68f528823a573d41ecdc5471bda51957a9
YA <9039417+yzkn@users.noreply.github.com>
2024-07-04 05:42:03 +0900

    Add Item.swift

---
+++ GPSLogger2/Item.swift
@@ -0,0 +1,48 @@
+//
+//  Item.swift
+//  GPSLogger2
+//
+//  Created by Yu on 2024/07/04.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Item {
+    // https://developer.apple.com/documentation/corelocation/cllocation
+    var title: String
+    var notes: String
+    var latitude: Double
+    var longitude: Double
+    var altitude: Double
+    var hAccuracy: Double
+    var vAccuracy: Double
+    var course: Double
+    var speed: Double
+    var timestamp: Date
+
+    init(
+        title: String,
+        notes: String,
+        latitude: Double,
+        longitude: Double,
+        altitude: Double,
+        hAccuracy: Double,
+        vAccuracy: Double,
+        course: Double,
+        speed: Double,
+        timestamp: Date
+    ) {
+        self.title = title
+        self.notes = notes
+        self.latitude = latitude
+        self.longitude = longitude
+        self.altitude = altitude
+        self.hAccuracy = hAccuracy
+        self.vAccuracy = vAccuracy
+        self.course = course
+        self.speed = speed
+        self.timestamp = timestamp
+    }
+}
