8b460d3f01a89dd67cad6b660808c8766ad8243d
YA <9039417+yzkn@users.noreply.github.com>
2024-07-04 05:55:49 +0900

    Update ContentView.swift

--- GPSLogger2/ContentView.swift
+++ GPSLogger2/ContentView.swift
@@ -5,15 +5,26 @@
 //  Created by Yu on 2024/07/04.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Query private var items: [Item]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("GPSLogger2")
+            
+            List(items) { item in
+                VStack(alignment: .leading) {
+                    Text(item.title)
+                    Text(item.timestamp, style: .date)
+                        .foregroundColor(.secondary)
+                }
+            }
         }
         .padding()
     }
@@ -21,4 +32,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .modelContainer(for: Item.self)
 }
